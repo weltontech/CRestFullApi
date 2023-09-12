156882ad9c5528547f4e4ac7e7cd331c0a5ce330
weltontech <weltonjc1@hotmail.com>
2023-09-12 15:56:22 -0300

    Add project files.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+
+    }
+
+}
