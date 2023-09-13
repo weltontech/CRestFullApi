02ae8198d412850e894601ab7ab7a15cb59e5003
weltontech <weltonjc1@hotmail.com>
2023-09-13 16:32:12 -0300

    Adicionado RecuperarFilme co paginação

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -9,15 +9,30 @@ namespace FilmesApi.Controllers;
 public class FilmeController
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Genero);
         Console.WriteLine(filme.Duracao);
 
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilme([FromQuery] int skip = 0, [FromQuery] int take = 50) 
+    {
+        return filmes.Skip(skip).Take(take);
+
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,9 +1,20 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
 
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get ; set; }
+    
+    [Required(ErrorMessage = "O Genero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho não pode exceder 50 caracteres")]
     public string Genero { get; set; }
+    
+    [Required]
+    [Range(70,600, ErrorMessage ="A duração deve ter entre 70 a 600 minutos")]
     public int Duracao { get; set; }
+   
 }
