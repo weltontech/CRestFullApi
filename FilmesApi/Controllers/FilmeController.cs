929c58bcc72024c434170a7846bd9a0dab3c8bb7
weltontech <weltonjc1@hotmail.com>
2023-09-14 17:18:31 -0300

    Padronizando Retorno REST

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -6,33 +6,35 @@ namespace FilmesApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class FilmeController
+public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
     private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
         filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Genero);
-        Console.WriteLine(filme.Duracao);
+        //O padrão REST pede que ao adicionar um objeto, voce retorne o objeto e o caminho deste objeto
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new {id = filme.Id}, filme);
 
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilme([FromQuery] int skip = 0, [FromQuery] int take = 50) 
+    public IEnumerable<Filme> RecuperaFilme([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
         return filmes.Skip(skip).Take(take);
 
     }
 
     [HttpGet("{id}")]
-    public Filme? RecuperaFilmePorId(int id)
+    public IActionResult RecuperaFilmePorId(int id)
     {
-        return filmes.FirstOrDefault(filme => filme.Id == id);
+
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
     }
 
 }
