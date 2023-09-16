2e8097fe3f9f0a11455e98e5d8d36b47c358572f
weltontech <weltonjc1@hotmail.com>
2023-09-16 07:39:38 -0300

    Documentando com Swagger

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -23,8 +23,16 @@ public class FilmeController : ControllerBase
         _mapper = mapper;
     }
 
+    /// <summary>
+    /// Adiciona um filme ao banco de dados
+    /// </summary>
+    /// <param name="filmeDto">Obejto com os campos necessários para criação de
+    /// um filme </param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso a inserção seja feita com sucesso</response>
     
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
     {
         //Fazendo um mapeamento de um filme a partir de um filmeDTO
